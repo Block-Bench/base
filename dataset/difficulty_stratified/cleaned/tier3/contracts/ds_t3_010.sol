9e1947b31d8d4763fff4f1f38d543dbd43c013d4
D4n13l-D33 <abrahamdanielu793@gmail.com>
2026-01-03 15:20:47 +0000

    removed fixed version from tier 3

--- dataset/difficulty_stratified/cleaned/tier3/contracts/ds_t3_010.sol
+++ dataset/difficulty_stratified/cleaned/tier3/contracts/ds_t3_010.sol
@@ -19,22 +19,3 @@ contract SimpleBank {
         payable(msg.sender).transfer(amount);
     }
 }
-
-contract SimpleBankB {
-    mapping(address => uint) private balances;
-
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
-    }
-
-    function getBalance() public view returns (uint) {
-        return balances[msg.sender];
-    }
-
-    function withdraw(uint amount) public {
-        require(balances[msg.sender] >= amount);
-        balances[msg.sender] -= amount;
-        (bool success, ) = payable(msg.sender).call{value: amount}("");
-        require(success, " Transfer of ETH Failed");
-    }
-}
--- dataset/difficulty_stratified/cleaned/tier3/contracts/ds_t3_019.sol
+++ dataset/difficulty_stratified/cleaned/tier3/contracts/ds_t3_019.sol
@@ -20,26 +20,4 @@ contract EtherStore {
     }
 }
 
-contract EtherStoreB {
-    mapping(address => uint256) public balances;
-    bool internal locked;
-
-    modifier checkLock() {
-        require(!locked, "Locked");
-        locked = true;
-        _;
-        locked = false;
-    }
-
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
-    }
-
-    function withdrawFunds(uint256 _weiToWithdraw) public checkLock {
-        require(balances[msg.sender] >= _weiToWithdraw);
-        balances[msg.sender] -= _weiToWithdraw;
-        (bool send, ) = msg.sender.call{value: _weiToWithdraw}("");
-        require(send, "send failed");
-    }
-}
 
